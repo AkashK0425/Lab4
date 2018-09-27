0257e64df7842b1ae11cbb754866c07f4b2632bb
AkashK0425 <akashk2@illinois.edu>
2018-09-26 20:07:57 -0500

    finished printWhoIsAhead and determineWinner functions

--- src/main/java/Colosseum.java
+++ src/main/java/Colosseum.java
@@ -90,7 +90,13 @@ public class Colosseum {
      * Implement this function.
      */
     public static void printWhoIsAhead() {
-        System.out.println("Implement me!");
+        if (firstPokemon.hitPoints > secondPokemon.hitPoints) {
+            System.out.println(firstPokemon.name + " is currently ahead!");
+        } else if (firstPokemon.hitPoints < secondPokemon.hitPoints) {
+            System.out.println(secondPokemon.name + " is currently ahead!");
+        } else {
+            System.out.println("Both Pokemon are at a draw!");
+        }
     }
 
     /**
@@ -101,7 +107,11 @@ public class Colosseum {
      * Write this function.
      */
     public static void determineWinner() {
-        System.out.println("Implement me!");
+        if (firstPokemon.hitPoints == 0) {
+            System.out.println(secondPokemon.name + " wins!");
+        } else {
+            System.out.println(firstPokemon.name + " wins!");
+        }
     }
 
     /**
